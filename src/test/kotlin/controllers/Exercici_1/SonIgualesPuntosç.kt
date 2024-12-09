ebcb754089d51c41a23b3880661ebc34cd9e0f6a
Cuenquis777 <marc.cuenca.7e8@itb.cat>
2024-12-09 23:44:01 +0100

    Commit:

--- src/test/kotlin/controllers/Exercici_1/SonIgualesPuntosç.kt
+++ src/test/kotlin/controllers/Exercici_1/SonIgualesPuntosç.kt
@@ -1,12 +1,59 @@
 package controllers.Exercici_1
 
+import controllers.ex1.Punt
 import org.junit.jupiter.api.Test
-
 import org.junit.jupiter.api.Assertions.*
 
 class SonIgualesPuntos {
 
     @Test
-    fun sonIguals() {
+    fun sonIguals1() {
+        //Creamos dos puntos con diferentes cordenadas
+        val esperado = Punt(3.0f, 3.0f)
+        val esperado2 = Punt(3.0f, 3.0f)
+
+
+        assertEquals(esperado, esperado2)
+
+        //Creamos otras dos puntos con diferentes coordenadas
+        val esperado3 = Punt(3.0f, 3.0f)
+        val esperado4 = Punt(3.0f, 3.0f)
+
+
+        assertEquals(esperado3, esperado4)
+    }
+
+    @Test
+    fun sonIguals2() {
+        //Creamos dos puntos con diferentes cordenadas
+        val esperado = Punt(9999.0f, 9999.0f)
+        val esperado2 = Punt(9999.0f, 9999.0f)
+
+
+        assertEquals(esperado, esperado2)
+
+        //Creamos otras dos puntos con diferentes coordenadas
+        val esperado3 = Punt(-9999.0f, -9999.0f)
+        val esperado4 = Punt(-9999.0f, -9999.0f)
+
+
+        assertEquals(esperado3, esperado4)
+    }
+
+    @Test
+    fun sonIguals3() {
+        //Creamos dos puntos con diferentes cordenadas
+        val esperado = Punt(999999.0f, 999999.0f)
+        val esperado2 = Punt(999999.0f, 999999.0f)
+
+
+        assertEquals(esperado, esperado2)
+
+        //Creamos otras dos puntos con diferentes coordenadas
+        val esperado3 = Punt(-999999.0f, -999999.0f)
+        val esperado4 = Punt(-999999.0f, -999999.0f)
+
+
+        assertEquals(esperado3, esperado4)
     }
 }
