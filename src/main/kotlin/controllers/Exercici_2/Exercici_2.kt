5f647bb259aaca434a737e173c09ecfa30e1ba67
denisitoow <denis.coello.7e8@itb.cat>
2024-12-09 12:11:23 +0100

    Ejercicio 2

--- src/main/kotlin/controllers/Exercici_2/Exercici_2.kt
+++ src/main/kotlin/controllers/Exercici_2/Exercici_2.kt
@@ -1,4 +1,36 @@
 package controllers.Exercici_2
 
+import java.util.Scanner
 
+fun main() {
+
+}
+
+fun menu(eleccion : Int) {
+    println("que quiere hacer?")
+}
+
+fun buidarSala(sala : Array<CharArray>){
+   for (fila in sala.indices){
+       for (butaca in sala[fila].indices){
+           sala[fila][butaca] = '_'
+       }
+   }
+}
+
+fun asientosDisponibles(sala : Array<CharArray>){
+    println(sala)
+}
+
+fun reservaAsientos(sala : Array<CharArray>, scan : Scanner){
+    println("introduzca la fila donde desee sentarse:")
+    val fila = scan.nextInt()
+    println("introduzca la butaca donde desee sentarse:")
+    val butaca = scan.nextInt()
+    if (sala[fila][butaca] == '_'){
+        sala[fila][butaca] = 'X'
+        println("Su asiento se ha reservado")
+    }
+    else println("Ese asiento está ocupado")
+}
 
