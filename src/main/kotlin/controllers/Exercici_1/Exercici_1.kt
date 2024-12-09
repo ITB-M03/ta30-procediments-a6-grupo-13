55f3ac9b67115a374e3b9ace1edda995623be029
Cuenquis777 <marc.cuenca.7e8@itb.cat>
2024-12-09 13:57:59 +0100

    COMMIT: 1.4 por acabar

--- src/main/kotlin/controllers/Exercici_1/Exercici_1.kt
+++ src/main/kotlin/controllers/Exercici_1/Exercici_1.kt
@@ -3,59 +3,66 @@ import utilities.abrirScanner
 import utilities.cerrarScanner
 import java.util.*
 
-
-//Creamos la lista data class con sus valores
-data class Punt (
+//Creamos la data class con sus valores
+data class Punt(
     var x: Float,
     var y: Float
 )
 
-fun main(){
+fun main() {
+    //Abrimos el scanner
+    val scan: Scanner = abrirScanner()
 
-    //Llamamos a la funcion de abrir scannner
-    var scan : Scanner = abrirScanner()
+    //Pedimos los valores del punto inicial
+    val x = pedirNumero("Introduce la coordenada X:", scan)
+    val y = pedirNumero("Introduce la coordenada Y:", scan)
 
-    //Hacemos una funcion para pedir numero
-    var numero = pedirNumero("Introduce un numero:",scan)
-    var numero2 = pedirNumero("Introduce un numero:",scan)
+    // Creamos el punto inicial
+    val punt = Punt(x, y)
 
-    // Creamos el punto
-    val punt = Punt(numero, numero2)
-    //Mostramos los puntos por pantalla
+    //Pedimos el desplazamiento
+    val a = pedirNumero("Introduce el desplazamiento en X:", scan)
+    val b = pedirNumero("Introduce el desplazamiento en Y:", scan)
+
+    //Mostramos el punto inicial
+    println("Punto inicial:")
     mostrarPuntos(punt)
 
+    //Realizamos la translación
+    trasladarPunto(punt, a, b)
 
-    var numero_suma = pedirNumero("Introduce un numero:",scan)
-    var numero2_suma = pedirNumero("Introduce un numero:",scan)
+    //Mostramos el punto final
+    println("Punto después de la translación:")
+    mostrarPuntos(punt)
 
+    val multi = pedirNumero("Introduce el numero de escala:", scan)
+    escalado(punt,multi)
+    mostrarPuntos(punt)
 
-    suma(numero, numero2, numero_suma,numero2_suma)
-
-    //Llamamos a la funcion de cerrar scanner
+    //Cerramos el scanner
     cerrarScanner(scan)
 }
 
-//Funcion para pedir numero
-fun pedirNumero (msg: String, scan: Scanner) : Float{
-    //Imprimimos el mensaje
+
+//Función para pedir un número
+fun pedirNumero(msg: String, scan: Scanner): Float {
     print(msg)
-    //Devolvemos el numero
     return scan.nextFloat()
 }
 
-//Creamos la funcion mostrarPuntos
-fun mostrarPuntos (punt: Punt)  {
-    var puntos = String.format("%.5f %.5f ", punt.x, punt.y).toMutableList()
-    return println(puntos)
+//Función para mostrar puntos
+fun mostrarPuntos(punt: Punt) {
+    println(String.format("(%.5f, %.5f)", punt.x, punt.y))
 }
 
-//Creamos la funcion de suma
-fun suma (x:Float, y: Float, a:Float, b:Float) {
-
-    var sum1= x+a
-    var sum2= y+b
-    var puntos = String.format("%.5f %.5f ", a, b, c, d).toMutableList()
+//Funcion para hacer la suma y transladarlos
+fun trasladarPunto(punt: Punt, a: Float, b: Float) {
+    punt.x += a
+    punt.y += b
 }
 
-
-
+//Funcion para hacer la suma y transladarlos
+fun escalado(punt: Punt, factor: Float) {
+    punt.x *= factor
+    punt.y *= factor
+}
