e51e695aaa2e0823b597a4635a5ed53c5a6a8b4b
denisitoow <denis.coello.7e8@itb.cat>
2024-12-09 13:52:09 +0100

    Test ex 2

---
+++ src/test/kotlin/controllers/Exercici_2/Exercici_2KtTest.kt
@@ -0,0 +1,11 @@
+package controllers.Exercici_2
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+ class Exercici_2KtTest {
+
+@Test
+ fun main() {}
+}
+//Todo: No podemos hacer test porque las funciones que nos devuelven cosas no son funciones que se puedan testear
